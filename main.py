d06a4c49169c43e18b67b23a4ea11b717a4b7a8b
Odenyi <brianodenyi@gmail.com>
2023-05-09 15:48:44 +0300

    add drawshape

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import random
 from turtle import Turtle,Screen
-timmy_the_turtle =Turtle
+
+tim =Turtle()
+# tim.shape("turtle")
+# tim.color("red")
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+# draw dots
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+# draw shapes from 3 to 10
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+# color = ["steel blue","light sea green" ,"sienna","blue","orange","dark cyan","deep pink",]
+# def drawshape(number_of_sides):
+#     angle = 360/number_of_sides
+#     for _ in range(number_of_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+# for shape in range(3,11):
+#     tim.color(random.choice(color))
+#     drawshape(shape)
+#random walk path
+
 
 
 

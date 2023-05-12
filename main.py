4a1962a8d28cd0ff88e2c51ea31ca9077ace6c0c
Odenyi <brianodenyi@gmail.com>
2023-05-12 17:14:33 +0300

    husk painting

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import random
 # from turtle import Turtle,Screen
 import turtle as t
-
+import colorgram
+colors = colorgram.extract("hirst.jpg", 30)
+rgb_color = []
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb_color.append((r,g,b))
+print(rgb_color)
 tim =t.Turtle()
 t.colormode(255)
+tim.penup()
+tim.speed("fastest")
+tim.hideturtle()
+tim.setheading(227)
+tim.forward(310)
+tim.setheading(0)
+dots_number = 100
+for dot_number in range(1,dots_number+1):
+    tim.dot(20, random.choice(rgb_color))
+    tim.forward(50)
+    if dot_number %10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
 # tim.shape("turtle")
 # tim.color("red")
 # for _ in range(4):
@@ -31,20 +56,25 @@ color = ["steel blue","light sea green" ,"sienna","blue","orange","dark cyan","d
 #random walk path
 #draw a random path
 
-def choose_color():
-    r = random.randint(0,255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    mytyple = (r,g,b)
-    return mytyple
-direction = [0,90,180,270]
-tim.speed("fastest")
-for _ in range(200):
-    tim.width(_/50)
-    tim.color(choose_color())
-    tim.forward(10)
-    tim.setheading(random.choice(direction))
-
+# def choose_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     mytyple = (r,g,b)
+#     return mytyple
+# direction = [0,90,180,270]
+# tim.speed("fastest")
+# for _ in range(200):
+#     tim.width(_/50)
+#     tim.color(choose_color())
+#     tim.forward(10)
+#     tim.setheading(random.choice(direction))
+# def draw_spiral_graft(size_gap):
+#     for _ in range(int(360/size_gap)):
+#         tim.color(choose_color())
+#         tim.circle(100)
+#         tim.setheading(tim.heading()+size_gap)
+# draw_spiral_graft(2)
 
 
 
